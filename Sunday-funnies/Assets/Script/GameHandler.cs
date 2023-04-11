58baae7b131ad68ce786c3ee08b0df83206d751c
DePlurg <griffinfaye1@gmail.com>
2023-04-11 23:03:04 +0900

    CodeMonkey

--- Sunday-funnies/Assets/Script/GameHandler.cs
+++ Sunday-funnies/Assets/Script/GameHandler.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
 
 public class GameHandler : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("GameHandler.Start");
+       
     }
 
     // Update is called once per frame
